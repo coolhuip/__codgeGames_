c13abc23a475807e40bf08e2a1a15a76835a3788
Danny Han <cool.huip@gmail.com>
2023-06-01 18:21:19 -0400

    Complete setup.py

--- cg_Pong/setup.py
+++ cg_Pong/setup.py
@@ -3,7 +3,7 @@ import pygame as pg
 pg.init()
 
 # Font that is used to render the text
-font20 = pg.font.Font('freesnsbold.ttf', 20)
+font20 = pg.font.Font('freesansbold.ttf', 20)
 
 # RGB values of standard colors
 BLACK = (0, 0, 0)
@@ -16,3 +16,8 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 # Used to adjust the frame rate
 clock = pg.time.Clock()
 FPS = 30
+
+z = pg.Rect(1, 1, 1, 1)
+x = pg.draw.rect(screen, GREEN, z)
+print(type(z))
+print(type(x))
