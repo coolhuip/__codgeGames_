5ca0b88bce0d820dffe03fa8c5180adf4a47cdf4
Danny Han <cool.huip@gmail.com>
2023-06-01 18:36:36 -0400

    Import pygame as pg from setup.py

--- cg_Pong/main.py
+++ cg_Pong/main.py
@@ -1,5 +1,9 @@
 from setup import pg
 
+from setup import font20, screen, clock
+from setup import BLACK, WHITE, GREEN
+from setup import WIDTH, HEIGHT, FPS
+
 
 if __name__ == '__main__':
     main()
