e2973eb04d493daa30f47395d40c0d44fa2f5566
Danny Han <cool.huip@gmail.com>
2023-06-01 17:36:10 -0400

    Create main.py

---
+++ cg_Pong/main.py
@@ -0,0 +1,23 @@
+import pygame as pg
+
+pg.init()
+
+# Font that is used to render the text
+font20 = pg.font.Font('freesnsbold.ttf', 20)
+
+# RGB values of standard colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+
+# Basic parameters of the screen
+WIDTH, HEIGHT = 900, 600
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+# Used to adjust the frame rate
+clock = pg.time.Clock()
+FPS = 30
+
+
+if __name__ == '__main__':
+    main()
+    pg.quit()
